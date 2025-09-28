f4728c27a5ee73d2498bb21ad58e107e021f3172
anarchie <116115028+anarchie347@users.noreply.github.com>
2025-09-28 23:28:59 +0100

    fixed performance issues in long scrolling of memview

--- src/app/MemtapeRowDisplay.tsx
+++ src/app/MemtapeRowDisplay.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, KeyboardEvent, useState } from "react";
+import { ChangeEvent, CSSProperties, KeyboardEvent, useState } from "react";
 import { Grid } from "react-window";
 
 export function MemtapeRowDisplay({
@@ -8,11 +8,12 @@ export function MemtapeRowDisplay({
 }: MemtapeRowDisplayProps) {
   const cellsPerBlock = blockDescription.length;
   const blockedVals = Array.from({
-    length: Math.ceil(1000 /*memtape.length / cellsPerBlock*/),
+    length: Math.ceil(memtape.length / cellsPerBlock),
   }).map((_, i) => memtape.slice(i * cellsPerBlock, (i + 1) * cellsPerBlock));
   console.log(blockedVals.length);
   return (
-    <div className="p-2 bg-gray-800 rounded-xl border-2 border-cyan-800">
+    <div className="p-2 bg-gray-800 rounded-xl border-2 border-cyan-800 h-50">
+      {/**TODO: remove hardcoded height */}
       <Grid
         cellComponent={GridCellComponent}
         cellProps={{
@@ -20,9 +21,9 @@ export function MemtapeRowDisplay({
           updateCellVal,
           cellsPerBlock,
           columnIndex: undefined as never, //for TS linting
-          rowIndex: undefined as never,
+          style: undefined as never,
         }}
-        columnCount={1000}
+        columnCount={blockedVals.length}
         rowCount={1}
         rowHeight="100%"
         columnWidth={200}
@@ -30,36 +31,25 @@ export function MemtapeRowDisplay({
     </div>
   );
 }
-
-/*blockedVals.map((b, i) => (
-        <div className="flex-none p-2" key={i}>
-          <MemBlock
-            values={b}
-            index={i}
-            updateMemCell={(nv, co) =>
-              updateCellVal(nv, i * cellsPerBlock + co)
-            }
-          />
-        </div>
-      ))*/
-
 function GridCellComponent({
-  rowIndex,
   columnIndex,
   blockedVals,
   updateCellVal,
   cellsPerBlock,
+  style,
 }: GridCellComponentProp) {
+  console.log(JSON.stringify(style));
   return (
-    <div className="JOO p-2">
-      {`${rowIndex}.${columnIndex}`}
-      <MemBlock
-        values={blockedVals[columnIndex]}
-        index={columnIndex}
-        updateMemCell={(nv, co) =>
-          updateCellVal(nv, columnIndex * cellsPerBlock + co)
-        }
-      />
+    <div style={style}>
+      <div className="p-2">
+        <MemBlock
+          values={blockedVals[columnIndex]}
+          index={columnIndex}
+          updateMemCell={(nv, co) =>
+            updateCellVal(nv, columnIndex * cellsPerBlock + co)
+          }
+        />
+      </div>
     </div>
   );
 }
@@ -104,8 +94,8 @@ function MemBlock({ index, values, updateMemCell }: MemBlockProps) {
   );
 }
 export interface GridCellComponentProp {
-  rowIndex: number;
-  columnIndex: number;
+  style: CSSProperties; // auto-added by react-window
+  columnIndex: number; // auto-added by react-window
   blockedVals: number[][];
   updateCellVal: (newVal: number, index: number) => void;
   cellsPerBlock: number;
